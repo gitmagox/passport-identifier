8e9bf619262075a143324dfcb55dd69271594a81
gitmagox <46257902@qq.com>
2017-09-08 01:14:05 +0800

    优化算法

--- identifier.php
+++ identifier.php
@@ -9,17 +9,13 @@ print_r($str);
 //求当前值中有几票权
 function getONE( $number )
 {
-    $str = [];
-    for ($i=0; $i<=31; $i++){
-        $str[] =  pow(2,$i);
-    }
     $keylist = [];
     for($i=0; $i<=31; $i++)
     {
         if( ($number<<$i) &  0x80000000)
-            $keylist[] = $str[31-$i];
+            $keylist[(31-$i)] = pow(2,(31-$i)) ;
     }
     return $keylist;
 }
 //test
-print_r($str-getONE( 48 ));
+print_r(getONE( 48 ));
