d067fa3b74f40febabf22bf6ac5099f2c98b9578
gitmagox <46257902@qq.com>
2017-09-10 20:49:21 +0800

    identifier helps

--- identifier.php
+++ identifier.php
@@ -1,34 +1,30 @@
 <?php
-/**
- * 根据权限值，取权限票
- * @param $number
- * @return array
- */
-function getOne( $number )
+
+
+trait  PermissionIdentifier
 {
-    $keylist = [];
-    for($i=0; $i<=31; $i++)
+    /**
+     * Parse Authority Identifier
+     */
+    function parseAuthorityIdentifier( $number )
     {
-        (($number<<$i) & 0x80000000) && ($keylist[(31-$i)] = pow(2,(31-$i)));
+        $keylist = [];
+        for($i=0; $i<=31; $i++)
+        {
+            (($number<<$i) & 0x80000000) && ($keylist[(31-$i)] = pow(2,(31-$i)));
+        }
+        return $keylist;
     }
-    return $keylist;
+
+    /**
+     *  Get one ticket authority Identifie
+     */
+    function getOneTicketFromAuthority( $number )
+    {
+        $one = array_pop( getOne($number) );
+        $number = $number  & ~$one;
+        return $one;
+    }
+
 }
 
-/**
- * 消费一票权限
- * @param $number
- * @return mixed
- */
-function payOne( &$number )
-{
-    $one = array_pop( getOne($number) );
-    $number = $number  & ~$one;
-    return $one;
-}
-//test
-$a = 48;
-echo $a;
-$b = payOne($a);
-echo($a);
-echo "\n";
-echo($b);
