2380fee9375f17af1b3889372b313d2cf386e673
gitmagox <46257902@qq.com>
2017-09-11 23:30:55 +0800

    laravel passport provider

---
+++ src/Provider/LaravelPassportProvider.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Gitmagox\Identifier\Providers;
+
+class LaravelPassportProvider extends AbstractServiceProvider
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function boot()
+    {
+
+    }
+
+    /**
+     * Alias the middleware.
+     *
+     * @return void
+     */
+    protected function aliasMiddleware()
+    {
+
+    }
+}
